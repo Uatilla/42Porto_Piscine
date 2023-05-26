324e689d1ee33b3931dc99e73888055dce82d819
Uatilla Viana Almeida <uviana-a@c1r4s2.42porto.com>
2023-05-26 16:17:14 +0100

    added ft_printf %p, %x and %X

--- Common_Core/ft_printf/ft_printf.c
+++ Common_Core/ft_printf/ft_printf.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: uviana-a <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/26 16:02:49 by uviana-a          #+#    #+#             */
+/*   Updated: 2023/05/26 16:02:53 by uviana-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
@@ -13,12 +25,12 @@ int	ft_putstr(char *str)
 		write(1, &str[i], 1);
 		i++;
 	}
-    return(i);
+	return (i);
 }
 
 int	ft_putchar(const char c)
 {
-	return(write(1, &c, 1));
+	return (write(1, &c, 1));
 }
 
 size_t	ft_strlen(const char *s)
@@ -32,54 +44,97 @@ size_t	ft_strlen(const char *s)
 	}
 	return (i);
 }
-int    flag_conversor(char flag, va_list args)
+
+int	ft_hex_base(unsigned long decimal_address, int flag)
 {
-    int letter_counter = 0;
-    if(flag == 'c')
-        letter_counter += ft_putchar(va_arg(args, int));//I CAN'T USE FT_PUTCHAR_FD BECAUSE IT RETURNS NOTHING.
-    if(flag == 's')
-    
-        letter_counter += ft_putstr(va_arg(args, char *));
-    else
-    {
-        //WHAT TO PRINT WHEN THE FLAG IS NOT IN THE MANDATORY PART?
-    }
+	int	letter_counter;
+	char		*base;
+
+	letter_counter = 0;
+	if (flag == 'X')
+		base = "0123456789ABCDEF";
+	if (flag == 'x')
+		base = "0123456789abcdef";
+	if (decimal_address >= 16)
+	{
+		letter_counter += ft_hex_base(decimal_address / 16, flag);
+		letter_counter += ft_hex_base(decimal_address % 16, flag);
+	}
+	else
+	{
+		letter_counter += ft_putchar(base[decimal_address]);
+	}
 	return (letter_counter);
 }
 
-int ft_printf(const char *str, ...)
+int	flag_conversor(char flag, va_list args)
 {
-    va_list args;
-    const char *ptr = str;
-    int letter_counter = 0;
+	int	letter_counter;
 
-    va_start(args, str);
-    while(*ptr != '\0')
-    {
-        if(*ptr == '%')
-        {
-            ptr++;
-            letter_counter += flag_conversor(*ptr, args);       
-        }
-        else
-        {
-            letter_counter += ft_putchar(*ptr);//I CAN'T USE FT_PUTCHAR_FD BECAUSE IT RETURNS VOID.
-        }
-        ptr++;
-    }
-    va_end(args);
-    return(letter_counter);
+	letter_counter = 0;
+	if (flag == '%')
+		letter_counter += ft_putchar('%');
+	if (flag == 'c')
+		letter_counter += ft_putchar(va_arg(args, int));
+	if (flag == 's')
+		letter_counter += ft_putstr(va_arg(args, char *));
+	if (flag == 'p')
+	{
+		letter_counter += ft_putstr("0x");
+		letter_counter += ft_hex_base(va_arg(args, unsigned long), 'x');
+	}
+	if (flag == 'x' || flag == 'X')
+		letter_counter += ft_hex_base(va_arg(args, unsigned long), flag);
+	else
+	{
+		write(1,"a",1);
+	}
+	return (letter_counter);
 }
 
-int main()
+int	ft_printf(const char *str, ...)
 {
-    char c_test = 'U';
-    char *s_test = "Uatilla";
+	va_list	args;
+	const char	*ptr = str;
+	int	letter_counter;
 
-    printf("%d\n",printf("Or_printf: [%c]\n", c_test));
-    printf("%d\n",ft_printf("ft_printf: [%c]\n", c_test));
-    printf("%d\n",printf("Or_printf: [%s]\n", s_test));
-    printf("%d\n",ft_printf("ft_printf: [%s]\n", s_test));
+	letter_counter = 0;
+	va_start(args, str);
+	while (*ptr != '\0')
+	{
+		if (*ptr == '%')
+		{
+			ptr++;
+			letter_counter += flag_conversor(*ptr, args);
+		}
+		else
+		{
+			letter_counter += ft_putchar(*ptr);
+		}
+		ptr++;
+	}
+	va_end(args);
+	return (letter_counter);
+}
 
-    return (0);
+int	main()
+{
+	char	c_test = 'U';
+	char	*s_test = "Uatilla";
+	char	*p_test = &*s_test;
+	int	xX_test = 123456789;
+
+	printf("%d\n", printf("Or_printf: [%c]\n", c_test));
+	printf("%d\n", ft_printf("ft_printf: [%c]\n", c_test));
+	printf("%d\n", printf("Or_printf: [%s]\n", s_test));
+	printf("%d\n", ft_printf("ft_printf: [%s]\n", s_test));
+	printf("%d\n", printf("Or_printf: [%p]\n", p_test));
+	printf("%d\n", ft_printf("ft_printf: [%p]\n", p_test));
+	printf("%d\n", printf("Or_printf: [%x]\n", xX_test));
+	printf("%d\n", ft_printf("ft_printf: [%x]\n", xX_test));
+	printf("%d\n", printf("Or_printf: [%X]\n", xX_test));
+	printf("%d\n", ft_printf("ft_printf: [%X]\n", xX_test));
+	printf("%d\n", printf("Or_printf: [%%]\n"));
+	printf("%d\n", ft_printf("ft_printf: [%%]\n"));
+	return (0);
 }
--- Common_Core/ft_printf/test.c
+++ Common_Core/ft_printf/test.c
@@ -3,6 +3,46 @@
 #include <stdarg.h>
 #include <unistd.h>
 
+int	ft_putchar(const char c)
+{
+	return (write(1, &c, 1));
+}
+
+int	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+    return(i);
+}
+
+int ft_hex_base(unsigned long decimal_address, int flag)
+{
+    int letter_counter;
+    char    *base;
+    
+    letter_counter = 0;
+    if (flag == 'X')
+        base = "0123456789ABCDEF";
+    if (flag == 'x')
+        base = "0123456789abcdef";
+    if (decimal_address >= 16)
+    {
+        letter_counter += ft_hex_base(decimal_address / 16, flag);
+        letter_counter += ft_hex_base(decimal_address % 16, flag);
+    }
+    else
+    {
+        letter_counter += ft_putchar(base[decimal_address]);
+    }
+    return(letter_counter);
+}
+
 int ft_printf(const char *str, ...)
 {
     va_list args;
@@ -15,11 +55,12 @@ int ft_printf(const char *str, ...)
         {
             str++;
             if (*str == 'p')
-                printf("Arg: %ld\n", va_arg(args, unsigned long));
-
+            {   letter_counter += ft_putstr("0x");
+                letter_counter += ft_hex_base(va_arg(args, unsigned long), 'x');            
+            }
         }
         else
-            write(1, &*str, 1);
+            letter_counter += write(1, &*str, 1);
         str++;
     }
     va_end(args);
@@ -31,8 +72,10 @@ int main()
     //char letter = 'U';
     char *name = "Uatilla";
     char *ptr = &*name;
-    ft_printf("Test%p", ptr);
-    printf("Pointer: %p\n", ptr);
+    int counter = printf("Or_printf: %p\n", ptr);
+    printf("%d\n", counter);
+    int counter2 = ft_printf("ft_printf: %p\n", ptr);
+    printf("%d\n", counter2);
     
 
 
