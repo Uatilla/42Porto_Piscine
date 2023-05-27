9feec85a7c9e052328e1b7df03d3de986598c0b6
Uatilla Viana Almeida <uviana-a@c1r12s14.42porto.com>
2023-05-27 17:25:32 +0100

    added %d and %i

--- Common_Core/ft_printf/ft_printf_func.c
+++ Common_Core/ft_printf/ft_printf_func.c
@@ -15,67 +15,6 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-//START FT_ITOA
-static	int	ft_get_len(long int n)
-{
-	long int	len;
-
-	len = 0;
-	if (n == 0)
-		return (1);
-	else if (n < 0)
-	{
-		n = n *(-1);
-		len++;
-	}
-	while (n > 0)
-	{
-		n = n / 10;
-		len++;
-	}
-	return (len);
-}
-
-static	char	*inp_itoa(long int num, int len, char *str_ft_itoa)
-{
-	int	i;
-
-	i = 1;
-	if (num == 0)
-		str_ft_itoa[0] = '0';
-	while (num > 0)
-	{
-		str_ft_itoa[len - i] = num % 10 + '0';
-		num = num / 10;
-		i++;
-	}
-	return (str_ft_itoa);
-}
-
-char	*ft_itoa(int n)
-{
-	char	*str_ft_itoa;
-	char	*str_ft_itoa2;
-	long	num;
-	int		len;
-
-	len = ft_get_len(n);
-	str_ft_itoa = (char *)malloc(sizeof(char) * (len + 1));
-	if (!str_ft_itoa)
-		return (NULL);
-	if (n < 0)
-	{
-		num = (long)n * (-1);
-		str_ft_itoa[0] = '-';
-	}
-	else
-		num = n;
-	str_ft_itoa2 = inp_itoa(num, len, str_ft_itoa);
-	str_ft_itoa2[len] = '\0';
-	return (str_ft_itoa2);
-}
-//END FT_ITOA
-
 int	ft_putstr(char *str)
 {
 	int	i;
@@ -128,10 +67,9 @@ int	ft_hex_base(unsigned long decimal_address, int flag)
 	return (letter_counter);
 }
 
-int	flag_conversor(char flag, va_list args)
+/*int	flag_conversor(char flag, va_list args)
 {
 	int	letter_counter;
-	char	*int_converted;
 
 	letter_counter = 0;
 	if (flag == '%')
@@ -147,12 +85,6 @@ int	flag_conversor(char flag, va_list args)
 	}
 	if (flag == 'x' || flag == 'X')
 		letter_counter += ft_hex_base(va_arg(args, unsigned long), flag);
-	if (flag == 'd' || flag == 'i')
-	{
-		int_converted = ft_itoa(va_arg(args, int));//If it don't work I can use the ft_putnbr() function.
-		ft_putstr(int_converted);
-		letter_counter += ft_strlen(int_converted);
-	}
 	else
 	{
 		//write(1,"a",1);
@@ -183,28 +115,6 @@ int	ft_printf(const char *str, ...)
 	}
 	va_end(args);
 	return (letter_counter);
-}
+}*/
 
-int	main()
-{
-	char	c_test = 'U';
-	char	*s_test = "Uatilla";
-	char	*p_test = &*s_test;
-	int	xX_test = 123456789;
 
-	printf("%d\n", printf("Or_printf: [%c]\n", c_test));
-	printf("%d\n", ft_printf("ft_printf: [%c]\n", c_test));
-	printf("%d\n", printf("Or_printf: [%s]\n", s_test));
-	printf("%d\n", ft_printf("ft_printf: [%s]\n", s_test));
-	printf("%d\n", printf("Or_printf: [%p]\n", p_test));
-	printf("%d\n", ft_printf("ft_printf: [%p]\n", p_test));
-	printf("%d\n", printf("Or_printf: [%x]\n", xX_test));
-	printf("%d\n", ft_printf("ft_printf: [%x]\n", xX_test));
-	printf("%d\n", printf("Or_printf: [%X]\n", xX_test));
-	printf("%d\n", ft_printf("ft_printf: [%X]\n", xX_test));
-	printf("%d\n", printf("Or_printf: [%%]\n"));
-	printf("%d\n", ft_printf("ft_printf: [%%]\n"));
-	printf("%d\n", printf("Or_printf: [%d]\n", -2147483647));
-	printf("%d\n", ft_printf("ft_printf: [%d]\n", -2147483647));
-	return (0);
-}
--- Common_Core/ft_printf/test.c
+++ Common_Core/ft_printf/test.c
@@ -3,7 +3,7 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-int	ft_putchar(const char c)
+/*int	ft_putchar(const char c)
 {
 	return (write(1, &c, 1));
 }
@@ -66,18 +66,91 @@ int ft_printf(const char *str, ...)
     va_end(args);
     return(letter_counter);
 }
+#include "libft.h"*/
 
-int main()
+/*static	int	ft_get_len(long int n)
 {
-    //char letter = 'U';
-    char *name = "Uatilla";
-    char *ptr = &*name;
-    int counter = printf("Or_printf: %p\n", ptr);
-    printf("%d\n", counter);
-    int counter2 = ft_printf("ft_printf: %p\n", ptr);
-    printf("%d\n", counter2);
-    
+	long int	len;
 
+	len = 0;
+	if (n == 0)
+		return (1);
+	else if (n < 0)
+	{
+		n = n *(-1);
+		len++;
+	}
+	while (n > 0)
+	{
+		n = n / 10;
+		len++;
+	}
+	return (len);
+}
+
+static	char	*inp_itoa(long int num, int len, char *str_ft_itoa)
+{
+	int	i;
+
+	i = 1;
+	if (num == 0)
+		str_ft_itoa[0] = '0';
+	while (num > 0)
+	{
+		str_ft_itoa[len - i] = num % 10 + '0';
+		num = num / 10;
+		i++;
+	}
+	return (str_ft_itoa);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*str_ft_itoa;
+	char	*str_ft_itoa2;
+	long	num;
+	int		len;
+
+	len = ft_get_len(n);
+	str_ft_itoa = (char *)malloc(sizeof(char) * (len + 1));
+	if (!str_ft_itoa)
+		return (NULL);
+	if (n < 0)
+	{
+		num = (long)n * (-1);
+		str_ft_itoa[0] = '-';
+	}
+	else
+		num = n;
+	str_ft_itoa2 = inp_itoa(num, len, str_ft_itoa);
+	str_ft_itoa2[len] = '\0';
+	return (str_ft_itoa2);
+}*/
+
+int main() {
+    unsigned int hypothesis1 = 0;
+    unsigned int hypothesis2 = -0;
+    unsigned int hypothesis3 = 10;
+    unsigned int hypothesis4 = -10;
+    unsigned int hypothesis5 = 4294967295;
+    //unsigned int hypothesis6 = -4294967294;
+    unsigned int hypothesis7 = 0xFFFFFFFF;
+    unsigned int hypothesis8 = 0xABCDEF;
+    unsigned int hypothesis9 = 0x0;
+    unsigned int hypothesis10 = 0x4D2;
+    unsigned int hypothesis11 = -0x4D2;
+
+    printf("1) 0: %u\n", hypothesis1);
+    printf("2) -0: %u\n", hypothesis2);
+    printf("3) 10: %u\n", hypothesis3);
+    printf("4) -10: %u\n", hypothesis4);
+    printf("5) 4294967295: %u\n", hypothesis5);
+    //printf("6) -4294967294: %u\n", hypothesis6);
+    printf("7) 0xFFFFFFFF: %u\n", hypothesis7);
+    printf("8) 0xABCDEF: %u\n", hypothesis8);
+    printf("9) 0x0: %u\n", hypothesis9);
+    printf("10) 0x4D2: %u\n", hypothesis10);    
+    printf("10) -0x4D2: %u\n", hypothesis11);  
 
     return (0);
 }
