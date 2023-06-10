04045bd28ccfb871982d39fc4b7861d435a0e918
Uatilla Viana Almeida <uviana-a@c1r8s16.42porto.com>
2023-06-10 14:51:03 +0100

    one trouble on the static variable

--- Common_Core/Get_next_line/get_next_line.c
+++ Common_Core/Get_next_line/get_next_line.c
@@ -31,6 +31,44 @@ size_t	ft_strlen(const char *s)
 	return (i);
 }
 
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	void	*ptr_substr;
+	size_t	i;
+
+	if (ft_strlen(s) - start < len)
+		len = ft_strlen(s) - start;
+	if (ft_strlen(s) < start)
+		len = 0;
+	ptr_substr = malloc (sizeof(char) * (len + 1));
+	if (!ptr_substr)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (i < len)
+	{
+		((char *)ptr_substr)[i] = s[start + i];
+		i++;
+	}
+	((char *)ptr_substr)[i] = '\0';
+	return ((char *)ptr_substr);
+}
+
+static	int	ft_strlen_mod2(const char *string, char search, int start_index)
+{
+	int	i;
+
+	i = start_index;
+	while (string[i] != search && string[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+
+
 char	*ft_strchr(const char *s, int c)
 {
 	int	i;
@@ -74,8 +112,18 @@ char	*ft_strjoin(char const *s1, char const *s2)
 }
 
 /*-------------------------------------------------------------*/
+char *ft_clean_line(char *line_to_clean)
+{
+    char    *line_cleaned;
 
-char *ft_get_content(int fd, char *line_cleaner)
+    line_cleaned = malloc(sizeof(char) * ft_strlen_mod2(line_to_clean, '\n', 0) + 1);
+    if (!line_cleaned)
+        return (NULL);
+    line_cleaned = ft_substr(line_to_clean, 0, ft_strlen_mod2(line_to_clean, '\n', 0));
+    return (line_cleaned);
+}
+
+char *ft_get_content(int fd, char *line_to_clean)
 {
     char    *buffer_content;
     int     bytes_read;
@@ -84,33 +132,45 @@ char *ft_get_content(int fd, char *line_cleaner)
     if (!buffer_content)
         return (NULL);
     bytes_read = 1;
-    //while (!ft_strchr(line_cleaner, '\n') && bytes_read != 0)
-    while (bytes_read != 0)//In the first while loop the \n will be find and the while loop will not run a second time?
+    while (!ft_strchr(line_to_clean, '\n') && bytes_read != 0)
     {
         bytes_read = read(fd, buffer_content, BUFFER_SIZE);
         if (bytes_read == -1)
         {
             free (buffer_content);
-            free (line_cleaner);
+            free (line_to_clean);
             return (NULL);
         }
         buffer_content[bytes_read] = '\0';
-        line_cleaner = ft_strjoin(line_cleaner, buffer_content);
+        /*if (!line_to_clean)
+        {
+            line_to_clean = malloc(sizeof(char) * 1);
+            line_to_clean[0] = '\0';
+        }*/
+        line_to_clean = ft_strjoin(line_to_clean, buffer_content);
     }
     free (buffer_content);
-    return (line_cleaner);
+    return (line_to_clean);
 }
 
 char *get_next_line(int fd)
 {
-    static char    *line_cleaner;
+    static char    *line_to_clean;
+    char *remainder;
+    char    *line_cleaned;
 
-    line_cleaner = "";// The string must be initialized because when running ft_strjoin inside the ft_get_content, it will return an error.
+    line_to_clean = "";// The string must be initialized because when running ft_strjoin inside the ft_get_content, it will return an error.
     if (BUFFER_SIZE < 0 || fd < 0)
         return (NULL);
-    line_cleaner = ft_get_content(fd, line_cleaner);
-    printf("line_cleaner: %s\n", line_cleaner);
-    return (line_cleaner);
+    line_to_clean = ft_get_content(fd, line_to_clean);
+    if (!line_to_clean)
+        return (NULL);
+    line_cleaned = ft_clean_line(line_to_clean);
+    remainder = ft_strchr(line_to_clean, '\n');
+    printf("line_to_clean: %s\n", line_to_clean);
+    printf("remainder: %s\n", remainder);
+    printf("line_cleaned: %s\n", line_cleaned);
+    return (line_cleaned);
 }
 int main()
 {
@@ -126,6 +186,10 @@ int main()
         return (1);
     }
     get_next_line(fd);
+    get_next_line(fd);
+    get_next_line(fd);
+    get_next_line(fd);
+
     close(fd);
     return (0);
 }
