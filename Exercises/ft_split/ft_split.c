a4b485d4a29645c7c09cd06257ff83bd1475c705
Uatilla Viana Almeida <uviana-a@c1r4s2.42porto.com>
2023-10-23 19:30:26 +0100

    added ft_split

--- Exercises/ft_split/ft_split.c
+++ Exercises/ft_split/ft_split.c
@@ -11,6 +11,59 @@
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int	ft_strlen_mod(int start, char *str)
+{
+	while (str[start] && str[start] != '\0' 
+		&& str[start] != 32 && str[start] != '\t' && str[start] != '\n')
+		start++;
+	return (start);
+}
+
+char	*ft_splitted(char *str, int start_index, int size)
+{
+	char	*word_splitted;
+	int		i;
+
+	word_splitted = malloc (sizeof(char *) * size);
+	if (!word_splitted)
+		return (NULL);
+	i = 0;
+	while (i < size)
+	{
+		word_splitted[i] = str[start_index + i];
+		i++;
+	}
+	word_splitted[i] = '\0';
+	return (word_splitted);
+}
+
+void	ft_fill_split(char **str_split, char *str)
+{
+	int	i;
+	int	st_idx;
+	int	e_idx;
+	int	j;
+
+	i = -1;
+	j = 0;
+	while (str[++i] && str[i] != '\0')
+	{
+		if (str[i] == 32 || str[i] == '\t' || str[i] == '\n')
+			;
+		else if ((str[i] != 32 || str[i] != '\n' || str[i] != '\t')
+			&& (i == 0 || str[i - 1] == 32 || str[i - 1] == '\n' 
+				|| str[i - 1] == '\t'))
+		{
+			st_idx = i;
+			e_idx = ft_strlen_mod(st_idx, str);
+			str_split[j] = ft_splitted(str, st_idx, e_idx - st_idx + 1);
+			j++;
+		}
+	}
+}
 
 int	check_word(char *str)
 {
@@ -22,33 +75,45 @@ int	check_word(char *str)
 	while (str[i] && str[i] != '\0')
 	{
 		if (str[i] == 32 || str[i] == '\t' || str[i] == '\n')
+			i++;
+		else if ((str[i] != 32 || str[i] != '\n' || str[i] != '\t') 
+			&& (i == 0 || str[i - 1] == 32 || str[i - 1] == '\n' 
+				|| str[i - 1] == '\t'))
 		{
-			if (str[i - 1] == 32 || str[i - 1] == '\t' || str[i - 1] == '\n')
-				word_started = 1;
+			words++;
+			i++;
 		}
 		else
-		{
-			if (
-		}
-		i++;
+			i++;
 	}
 	return (words);
-	
 }
 
 char	**ft_split(char *str)
 {
-	//1) Count Words:
-	int	wrd_count;
+	int		words;
+	char	**ptr_split;
 
-	wrd_count = check_word(text);
+	words = check_word(str);
+	ptr_split = malloc(sizeof(char *) * (words + 1));
+	if (!ptr_split)
+		return (NULL);
+	ft_fill_split(ptr_split, str);
+	return (ptr_split);
 }
-
-int	main()
+/*
+int	main(void)
 {
-	char	*text = "  Uatilla Viana Almeida";
+	char	*text = "  Uatilla Viana    Almeida    ";
 	char	**splitted;
+	int	i;
 
+	i = 0;
 	splitted = ft_split(text);
+	while (splitted[i] && splitted[i] != (void *)0)
+	{
+		printf("[%d] %s\n", i, splitted[i]);
+		i++;
+	}
 	return (0);
-}
+}*/
